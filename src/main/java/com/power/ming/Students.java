48ebaa22079baa36ea5769eb4d81b5228e255123
teacher <1782226497@qq.com>
2023-12-07 22:37:36 +0800

    first commit

---
+++ src/main/java/com/power/ming/Students.java
@@ -0,0 +1,13 @@
+package com.power.ming;
+
+/**
+ * @Description:
+ * @Author: Teacher 侬
+ * @Date: 2023/12/7 22:14
+ */
+
+public class Students {
+    public static void main(String[] args) {
+        System.out.println("hello,world");
+    }
+}
