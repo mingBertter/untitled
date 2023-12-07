99369a3fca574eb317c378f8fc81c1c012e465f5
teacher <1782226497@qq.com>
2023-12-07 22:44:17 +0800

    third commit

--- src/main/java/com/power/ming/Students.java
+++ src/main/java/com/power/ming/Students.java
@@ -10,5 +10,7 @@ public class Students {
     public static void main(String[] args) {
         System.out.println("hello,world");
         System.out.println("hello,git");
+        System.out.println("hello,git3");
+        System.out.println("hello,git321");
     }
 }
