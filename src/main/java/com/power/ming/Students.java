6a5566994c7d89c33bdfca9da0c97d8017ea595f
teacher <1782226497@qq.com>
2023-12-07 22:41:45 +0800

    second commit

--- src/main/java/com/power/ming/Students.java
+++ src/main/java/com/power/ming/Students.java
@@ -9,5 +9,6 @@ package com.power.ming;
 public class Students {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,git");
     }
 }
